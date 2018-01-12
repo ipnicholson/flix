91fb63952de8cce500b1208461d1cd60ad0471d1
Ian Nicholson <ipnicholson@gmail.com>
2018-01-12 11:21:48 -0800

    tie in to movies db

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,10 +1,5 @@
 class MoviesController < ApplicationController
   def index # this is the index 'action'
-    @movies = [
-      'Interstellar',
-      'Dunkirk',
-      'Damnation',
-      'Kubo and the Two Strings'
-    ]
+    @movies = Movie.all
   end
 end
