8629b64d653ea8eb9cacff7d2bea3c859bc9e232
Ian Nicholson <ipnicholson@gmail.com>
2018-01-12 16:25:07 -0800

    use format_total_gross view helper

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,9 @@
 module MoviesHelper
+  def format_total_gross(movie)
+    if movie.total_gross == 0
+      "N/A"
+    else
+      number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
 end
